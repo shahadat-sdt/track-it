dd7c8b0471a008860bd3f25436a618b337a97f69
Shahadat SDT <developer.shahadat@gmail.com>
2026-01-15 02:45:10 +0600

    Handle NewIssue form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,27 @@
+'use client'
 import {Button, TextArea, TextField} from "@radix-ui/themes";
+import {useForm} from "react-hook-form";
+import {useRouter} from "next/dist/client/components/navigation";
+
+interface NewIssueForm {
+    name: string;
+    description: string;
+}
 
 const NewIssuePage = () => {
+
+    const router = useRouter()
+
+    const {register, handleSubmit} = useForm<NewIssueForm>()
     return (
-        <div className="space-y-3 max-w-xl ">
-            <TextField.Root placeholder='Title'/>
-            <TextArea placeholder='Description'/>
+        <form className="space-y-3 max-w-xl " onSubmit={handleSubmit(async (data) => {
+            await fetch("/api/issues", {method: "POST", body: JSON.stringify(data)});
+            router.push("/issues");
+        })}>
+            <TextField.Root placeholder='Name' {...register("name")}/>
+            <TextArea placeholder='Description' {...register("description")}/>
             <Button>Submit New Issue</Button>
-        </div>
+        </form>
     );
 };
 
