9c449448d7adfadbe29a8ab8868f9ae05021f839
Shahadat SDT <developer.shahadat@gmail.com>
2026-01-15 21:35:45 +0600

    refactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -12,39 +12,45 @@ type NewIssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
 
-    const router = useRouter()
-    const [error, setError] = useState('')
-    const [isSubmitting, setIsSubmitting] = useState(false)
+        const router = useRouter()
+        const [error, setError] = useState('')
+        const [isSubmitting, setIsSubmitting] = useState(false)
 
-    const {register, handleSubmit, formState: {errors}} = useForm<NewIssueForm>({
-        resolver: zodResolver(createIssueSchema)
-    })
-    return (
-        <div className="max-w-xl">
 
-            {error && <Callout.Root color='red' className='mb-5'>
-                <Callout.Text>{error}</Callout.Text>
-            </Callout.Root>}
-            <form className="space-y-3" onSubmit={handleSubmit(async (data) => {
-                try {
-                    setIsSubmitting(true)
-                    const res = await fetch("/api/issues", {method: "POST", body: JSON.stringify(data)});
-                    if (!res.ok) throw new Error()
-                    router.push("/issues");
+        const {register, handleSubmit, formState: {errors}} = useForm<NewIssueForm>({
+            resolver: zodResolver(createIssueSchema)
+        })
 
-                } catch (e) {
-                    setIsSubmitting(false)
-                    setError("Unexpected error occurred.");
-                }
-            })}>
-                <TextField.Root placeholder='Name' {...register("name")}/>
-                <ErrorMessage>{errors.name?.message}</ErrorMessage>
-                <TextArea placeholder='Description' {...register("description")}/>
-                <ErrorMessage>{errors.description?.message}</ErrorMessage>
-                <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner/>}</Button>
-            </form>
-        </div>
-    );
-};
+        const onsubmit = handleSubmit(async (data) => {
+            try {
+                setIsSubmitting(true)
+                const res = await fetch("/api/issues", {method: "POST", body: JSON.stringify(data)});
+                if (!res.ok) throw new Error()
+                router.push("/issues");
+
+            } catch (e) {
+                setIsSubmitting(false)
+                setError("Unexpected error occurred.");
+            }
+        })
+        return (
+            <div className="max-w-xl">
+
+                {error && <Callout.Root color='red' className='mb-5'>
+                    <Callout.Text>{error}</Callout.Text>
+                </Callout.Root>}
+                <form className="space-y-3" onSubmit={onsubmit}>
+                    <TextField.Root placeholder='Name' {...register("name")}/>
+                    <ErrorMessage>{errors.name?.message}
+                    </ErrorMessage>
+                    <TextArea placeholder='Description' {...register("description")}/>
+                    <ErrorMessage>{errors.description?.message}</ErrorMessage>
+                    <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner/>}</Button>
+                </form>
+            </div>
+        )
+            ;
+    }
+;
 
 export default NewIssuePage;
