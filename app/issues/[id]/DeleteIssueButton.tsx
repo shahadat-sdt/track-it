be081365d89ac16320cdb60aa2854cdb728e267b
Shahadat SDT <developer.shahadat@gmail.com>
2026-02-12 18:16:13 +0600

    feat: handle issue deleting error

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,36 +1,58 @@
 'use client'
 import {AlertDialog, Button, Flex} from "@radix-ui/themes";
-import { useRouter} from "next/navigation";
+import {useRouter} from "next/navigation";
+import {useState} from "react";
 
-const DeleteIssueButton = ({issueId}: {issueId: number}) => {
+const DeleteIssueButton = ({issueId}: { issueId: number }) => {
     const router = useRouter()
-    return (
-        <AlertDialog.Root>
-            <AlertDialog.Trigger>
-                <Button color="red">
-                    Delete Issue
-                </Button>
-            </AlertDialog.Trigger>
-            <AlertDialog.Content>
-                <AlertDialog.Title>Delete Issue</AlertDialog.Title>
-                <AlertDialog.Description>Are you sure you want to delete this issue? This action cannot be undone.</AlertDialog.Description>
-                <Flex mt='3' gap='4'>
-                    <AlertDialog.Cancel>
-                        <Button variant='soft' color="gray">Cancel</Button>
-                    </AlertDialog.Cancel>
-                    <AlertDialog.Action>
-                        <Button color='red' onClick={async ()=>{
-                            await fetch("/api/issues/"+issueId, {
-                                method: "DELETE"
-                            })
-                            router.push("/issues")
-                            router.refresh()
-                        }}>Delete Issue</Button>
-                    </AlertDialog.Action>
-                </Flex>
-            </AlertDialog.Content>
-        </AlertDialog.Root>
+    const [error, setError] = useState(false)
+    const deleteIssue = async () => {
+        try {
+            await fetch("/api/issues/" + issueId, {
+                method: "DELETE"
+            })
+            router.push("/issues")
+            router.refresh()
+        } catch (error) {
+            setError(true)
+        }
 
+    }
+    return (
+        <>
+
+            <AlertDialog.Root>
+                <AlertDialog.Trigger>
+                    <Button color="red">
+                        Delete Issue
+                    </Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content>
+                    <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+                    <AlertDialog.Description>Are you sure you want to delete this issue? This action cannot be
+                        undone.</AlertDialog.Description>
+                    <Flex mt='3' gap='4'>
+                        <AlertDialog.Cancel>
+                            <Button variant='soft' color="gray">Cancel</Button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action>
+                            <Button color='red' onClick={deleteIssue}>Delete Issue</Button>
+                        </AlertDialog.Action>
+                    </Flex>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+            <AlertDialog.Root open={error}>
+                <AlertDialog.Content>
+                    <AlertDialog.Title>Error</AlertDialog.Title>
+                    <AlertDialog.Description>Could not delete this error</AlertDialog.Description>
+                    <AlertDialog.Action>
+                        <Button mt='2' color='gray' variant='soft' onClick={() => {
+                            setError(false)
+                        }}>Ok</Button>
+                    </AlertDialog.Action>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+        </>
     );
 };
 
