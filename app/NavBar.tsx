50be22c08d057f3d76388765e4d792bac40354e7
Shahadat SDT <developer.shahadat@gmail.com>
2026-02-13 18:38:00 +0600

    UI: change the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,26 +3,49 @@ import Link from "next/dist/client/link";
 import {AiFillBug} from "react-icons/ai";
 import classNames from "classnames";
 import {usePathname} from "next/dist/client/components/navigation";
+import {Box, Container, Flex} from "@radix-ui/themes";
+import {useSession} from "next-auth/react";
 
 const NavBar = () => {
     const currentPage = usePathname()
+    const {status, data: session} = useSession()
     const links = [
         {label: 'Dashboard', href: '/'},
         {label: 'Issues', href: '/issues'},
     ]
     return (
-        <nav
-            className="flex border-b border-gray-200 space-x-6 mb-5  px-5 h-14 items-center navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link className="navbar-brand" href="/"><AiFillBug/></Link>
-            <ul className="flex space-x-6">
-                {links.map((link) => (
-                    <Link className={classNames({
-                        "text-zinc-900" : currentPage === link.href,
-                        "text-zinc-500" : currentPage !== link.href,
-                        "hover:text-zinc-800 transition-colors" : true
-                    })} key={link.label}
-                          href={link.href}>{link.label}</Link>))}
-            </ul>
+        <nav className="border-b border-gray-200  mb-5  px-5 py-5 navbar navbar-expand-lg navbar-dark bg-dark">
+            <Container>
+                <Flex justify="between">
+                    <Flex align='center' gap='3'>
+                        <Link className="navbar-brand" href="/"><AiFillBug/></Link>
+                        <ul className="flex space-x-6">
+
+
+                            {links.map((link) => (
+                                <li key={link.href}>
+                                    <Link className={classNames({
+                                        "text-zinc-900": currentPage === link.href,
+                                        "text-zinc-500": currentPage !== link.href,
+                                        "hover:text-zinc-800 transition-colors": true
+                                    })} key={link.label}
+                                          href={link.href}>{link.label}</Link>
+                                </li>
+                            ))}
+
+                        </ul>
+                    </Flex>
+                    <Box>
+                        {status === "unauthenticated" && < Link href={"/api/auth/signin"}>
+                            Login
+                        </Link>}
+                        {status === "authenticated" && < Link href={"/api/auth/signout"}>
+                            Logout
+                        </Link>}
+                    </Box>
+                </Flex>
+
+            </Container>
         </nav>
     );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import {Geist, Geist_Mono, Inter} from "next/font/google";
 
 import NavBar from "@/app/NavBar";
 import {Container, Theme} from "@radix-ui/themes";
+import AuthProvider from "@/app/auth/Provider";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -35,17 +36,19 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={`$ ${inter.variable} antialiased`}>
-        <Theme appearance='light' accentColor="violet">
+        <AuthProvider>
+            <Theme appearance='light' accentColor="violet">
 
 
-            <NavBar/>
-            <main className='p-5'>
-                <Container>
-                    {children}
-                </Container>
-            </main>
+                <NavBar/>
+                <main className='p-5'>
+                    <Container>
+                        {children}
+                    </Container>
+                </main>
 
-        </Theme>
+            </Theme>
+        </AuthProvider>
         </body>
         </html>
     );
