e602e86df91579e318c4f693f8b144fb9eeab31f
Shahadat SDT <developer.shahadat@gmail.com>
2026-02-19 13:54:01 +0600

    UI: Build the Assignee selector component

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,6 +6,7 @@ import IssueDetails from "@/app/issues/[id]/IssueDetails";
 import DeleteIssueButton from "@/app/issues/[id]/DeleteIssueButton";
 import authOptions from "@/app/auth/AuthOptions";
 import {getServerSession} from "next-auth";
+import AssigneeSelect from "@/app/issues/[id]/edit/AssigneeSelect";
 
 
 interface Props {
@@ -32,6 +33,7 @@ const IssueDetailsPage = async ({params}: Props) => {
             <Box>
                 {session &&<Flex direction="column" gap='4'>
 
+                    <AssigneeSelect/>
                     <EditIssueButton issueId={issue.id}/>
                     <DeleteIssueButton issueId={issue.id}/>
                 </Flex>
