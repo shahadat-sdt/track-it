fc3b95a0305334eacb38fd1f9a4f5cd13847e591
Shahadat SDT <developer.shahadat@gmail.com>
2026-02-26 23:13:37 +0600

    feat: update the patch api to assign issue to user

--- app/ValidationSchemas.tsx
+++ app/ValidationSchemas.tsx
@@ -2,5 +2,11 @@ import {z} from "zod";
 
 export const issueSchema = z.object({
     name: z.string().min(1, 'Name is required').max(255),
-    description: z.string().min(1, "Description is required")
+    description: z.string().min(1, "Description is required").max(65535),
+})
+
+export const patchIssueSchema = z.object({
+    name: z.string().min(1, 'Name is required').max(255).optional(),
+    description: z.string().min(1, "Description is required").max(65535).optional(),
+    assignedToUserId: z.string().min(1, 'AssignedToUserId is required').max(255).optional().nullable(),
 })
--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import {issueSchema} from "@/app/ValidationSchemas";
+import {patchIssueSchema} from "@/app/ValidationSchemas";
 import {NextResponse} from "next/dist/server/web/spec-extension/response";
 import {prisma} from "@/client";
 import {NextRequest} from "next/dist/server/web/spec-extension/request";
@@ -8,14 +8,25 @@ import authOptions from "@/app/auth/AuthOptions";
 export async function PATCH(request: Request, {params}: { params: { id: string } }) {
 
     const session = await getServerSession(authOptions)
-    if (!session) return NextResponse.json({},{status: 401});
+    if (!session) return NextResponse.json({}, {status: 401});
 
 
     const param = await params
 
     const body = await request.json();
-    const validation = issueSchema.safeParse(body);
+
+    const validation = patchIssueSchema.safeParse(body);
     if (!validation.success) return NextResponse.json(validation.error.formErrors, {status: 400})
+
+    const {name, description, assignedToUserId} = body;
+
+    if (assignedToUserId) {
+       const user = await prisma.user.findUnique({where: {id: assignedToUserId}})
+        if (!user) {
+            return NextResponse.json({error: "Invalid user"}, {status: 400})
+        }
+    }
+
     const issue = await prisma.issue.findUnique({
         where: {id: parseInt(param.id)}
     })
@@ -24,8 +35,9 @@ export async function PATCH(request: Request, {params}: { params: { id: string }
     const updatedIssue = await prisma.issue.update({
         where: {id: parseInt(param.id)},
         data: {
-            name: body.name,
-            description: body.description,
+            name,
+            description,
+            assignedToUserId
         }
     })
 
@@ -35,8 +47,8 @@ export async function PATCH(request: Request, {params}: { params: { id: string }
 
 export async function DELETE(req: NextRequest, {params}: { params: { id: string } }) {
 
-     const session = await getServerSession(authOptions)
-    if (!session) return NextResponse.json({},{status: 401});
+    const session = await getServerSession(authOptions)
+    if (!session) return NextResponse.json({}, {status: 401});
 
     const param = await params
 
